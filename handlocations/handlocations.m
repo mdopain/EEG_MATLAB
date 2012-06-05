1d10d1123ab0c2b23a8f42c46b840915dff9d3f1
Daniel Groothuysen <d.groothuysen@student.utwente.nl>
2012-06-06 01:27:40 +0200

    making progress, using plugin gridfit

--- handlocations/handlocations.m
+++ handlocations/handlocations.m
@@ -1,4 +1,4 @@
-%   Script to generate sensitivity maps 
+%   Script to generate sensitivity maps
 
 
 
@@ -8,20 +8,20 @@ daniel_r = imread('hand_daniel_r.png');
 daniel_l = imread('hand_daniel_l.png');
 paul_r = imread('hand_paul_r.png');
 paul_l = imread('hand_paul_l.png');
-mask = imread('hand_mask.png');
-
+mask = double(rgb2gray(imread('hand_mask.png')));
+ 
 eval_images = {daniel_r,daniel_l,paul_r,paul_l};
 
 %% Now evaluate points
 pts = zeros(14,2,4);
 
-for ptnr=1:4;
-imshow(eval_images{ptnr});
-for loc=1:14;
-[pts(loc,1,ptnr),pts(loc,2,ptnr)]=ginput(1);
-hold on
-plot(x(ptnr,loc),y(ptnr,loc),'g*');
-end
+for i=1:4;
+imshow(eval_images{i});
+    for loc=1:14;
+    [pts(loc,1,i),pts(loc,2,i)]=ginput(1);
+    hold on
+    plot(pts(loc,1,i),pts(loc,2,i),'g*');
+    end
 end
 
 %% Import data from excel sheet
@@ -29,19 +29,37 @@ end
 % This is the baseline measurement of the first of June, with Daniël en
 % Paul as test subjects.
 
-% The data is structured, row after row, like this:
-% 1. daniel_l 
-% 2. daniel_r 
-% 3. paul_l 
-% 4. paul_r
+% The data is structured, row after row, like this: 1. daniel_l 2. daniel_r
+% 3. paul_l 4. paul_r
 
 importdata = xlsread('importfile_lokaties_PT.xlsx');
 
 %% Now to combine the positions with the perceptual thresholds
-[X Y] = meshgrid(1:398,1:480);
+
+% The following loop results in a threedimensional array 'data' which
+% contains X and Y coordinates in the first two columns, and a
+% corresponding perceptual threshold value in the third column in [mA]. The
+% third dimension is for selecting other hands. The indices are as
+% follows:
+
+% 1. daniel_l 
+% 2. daniel_r
+% 3. paul_l 
+% 4. paul_r
 
 for i=1:4
-Z(i) = 
+data(:,:,i) = [pts(:,:,i),transpose(importdata(i,:))];
 end
+%% 
+tic
+[xr,yr,zr]=gridfit(data(:,1,1),data(:,2,1),data(:,3,1),1:398,1:480);
+toc
+%% Try to plot
+image = imagesc(xr);
+alpha(mask,'color');
+hold on
+image(image.*mask);
+
+axis equal
 
 
