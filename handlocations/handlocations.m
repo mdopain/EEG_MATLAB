af06b0479be9641227716bd34d58a609349bf79e
Daniel Groothuysen <d.groothuysen@student.utwente.nl>
2012-06-05 15:19:11 +0200

    Working on the handlocation script

--- handlocations/handlocations.m
+++ handlocations/handlocations.m
@@ -3,10 +3,41 @@
 
 %% First, load the images
 
-base = imread('hand_normal.png');
+daniel_r = imread('hand_daniel_r.png');
+daniel_l = imread('hand_daniel_l.png');
+paul_r = imread('hand_paul_r.png');
+paul_l = imread('hand_paul_l.png');
 mask = imread('hand_mask.png');
 
-%% Now evaluate points
+eval_images = {daniel_r,daniel_l,paul_r,paul_l};
 
-imshow(base);
-[x1,y1]=ginput(14);
+%% Now evaluate points
+x = zeros(4,14);
+y = zeros(4,14);
+
+
+for i=1:4;
+imshow(eval_images{i});
+for j=1:14;
+[x(i,j),y(i,j)]=ginput(1);
+hold on
+plot(x(i,j),y(i,j),'g*');
+end
+end
+
+%% Import data from excel sheet
+
+% This is the baseline measurement of the first of June, with Daniël en
+% Paul as test subjects.
+
+% The data is structured in this way:
+% [daniel_l,daniel_r,paul_l,paul_r]
+data = xlsread('importfile_lokaties_PT.xlsx');
+
+%% Now to combine the positions with the perceptual thresholds
+
+
+
+
+
+
